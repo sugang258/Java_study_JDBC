cf2f4b143b83256a4baacca3cf154e66abc257cb
Hongsugyeong <olivia258@naver.com>
2022-07-27 12:20:24 +0900

    test

--- src/com/gang/test/TestMain.java
+++ src/com/gang/test/TestMain.java
@@ -1,9 +1,19 @@
 package com.gang.test;
 
+import com.gang.util.DBConnector;
+
 public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		DBConnector dbConnector = new DBConnector();
+		try {
+			dbConnector.getConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
